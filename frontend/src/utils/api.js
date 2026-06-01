54fcf79d0644c7e3811eee940039dd7b9168f419
Sandhya Bomjan <tsandhya856@gmail.com>
2026-06-01 14:45:12 +1000

    Fix frontend API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://focusflow-mvp.vercel.app";
 
 export const getTasks = async () => {
   const response = await fetch(`${API_URL}/tasks`);
